fe193bbb7a387e0b1bb8013bcbe068baaa69c6f9
Vishal Patel <85049843+venteury@users.noreply.github.com>
2025-01-17 23:49:46 +0530

    db connection, pooled connetion , signup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,68 @@
+import { PrismaClient } from "@prisma/client/edge"; // Use the Prisma Client for edge
+import { withAccelerate } from "@prisma/extension-accelerate"; // Import Prisma Accelerate extension
 import { Hono } from "hono";
+import { sign } from "hono/jwt";
+import { z } from "zod";
 
-const app = new Hono();
+const app = new Hono<{
+  Bindings: {
+    DATABASE_URL: string;
+    JWT_SECRET: string;
+  };
+}>();
 
-app.post("/api/v1/signup", (c) => {
-  return c.text("Hello Hono!");
+// const prisma = new PrismaClient({
+//   datasourceUrl: env.DATABASE_URL,
+// }).$extends(withAccelerate());
+
+//  avoid using global varialble in serverless environment as you might miss the context
+
+// app.use("*", (c, next) => {
+//   const jwtMiddleware = jwt({
+//     secret: c.env.JWT_SECRET,
+//   });
+//   return jwtMiddleware(c, next);
+// });
+
+// Input validation schema using zod
+const userSchema = z.object({
+  email: z.string().email(),
+  name: z.string().min(1),
+  password: z.string().min(8),
+});
+
+app.post("/api/v1/signup", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  try {
+    const body = await c.req.json();
+    const validationResult = userSchema.safeParse(body);
+
+    if (!validationResult.success) {
+      return c.json({ error: "Invalid input" }, 400);
+    }
+
+    const user = await prisma.user.create({
+      data: {
+        email: body.email,
+        name: body.name,
+        password: body.password,
+      },
+    });
+
+    const payload = { email: user.email, id: user.id };
+
+    const token = await sign(payload, c.env.JWT_SECRET);
+
+    return c.json({ success: true, jwt: token });
+  } catch (error) {
+    console.error(error);
+    return c.json({ error: "Internal server error" }, 500);
+  } finally {
+    await prisma.$disconnect();
+  }
 });
 
 app.post("/api/v1/signin", (c) => {
